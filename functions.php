d9323b0c144e0bea364b5b19ff30dda3259c5e5e
Greg Rickaby <gregrickaby@gmail.com>
2016-02-19 07:52:16 -0600

    add svgs, move js into its own file

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@
  */
 function grd_enqueue_scripts() {
 	wp_enqueue_script( 'wp-util' );
+	wp_enqueue_script( 'grd-weather', get_template_directory_uri() . '/assets/js/weather.js', array( 'jquery' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'grd_enqueue_scripts' );
 
@@ -31,6 +32,8 @@ function grd_do_weather_template() { ?>
 			<div class="entry-content">
 				<p class="current-conditions">{{ data.currently.summary }}, {{ data.currently.temperature }}F</p>
 
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/{{ data.currently.icon }}.svg" />
+
 				<h3><?php esc_html_e( 'Next 24 Hours', 'grd-weather' ); ?></h3>
 				<p class="today">{{ data.hourly.summary }}</p>
 
@@ -44,40 +47,3 @@ function grd_do_weather_template() { ?>
 
 <?php }
 add_action( 'grd_content', 'grd_do_weather_template', 25 );
-
-
-/**
- * Put our JS data into the weather template.
- */
-function grd_print_scripts() { ?>
-
-	<script type="text/javascript">
-		jQuery( document ).ready( function($) {
-
-			// Set global variables.
-			var apiUrl = 'https://api.forecast.io/forecast/62627807ae3841ba587c80d49b90759b/31.3601,-85.8554';
-				postTemplate = wp.template( 'grd-weather' );
-				siteContent = $( '.site-content' );
-
-			// Build the template using API data.
-			var buildTemplate = function( data ) {
-				siteContent.append( postTemplate( data ) );
-			}
-
-			// Get data from API.
-			var fetchData = $.ajax({
-				dataType: "jsonp",
-				url: apiUrl,
-
-			// When finished, pass data to template.
-			}).done( function( data )  {
-				buildTemplate( data );
-			});
-
-			// Engage!
-			fetchData;
-
-		});
-	</script>
-<?php }
-add_action( 'wp_footer', 'grd_print_scripts', 25 );
